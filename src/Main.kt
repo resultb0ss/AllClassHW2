fa1b45e48ebd444f9120ec7ea03f069d3efe0e06
Admin <reteamboss@gmail.com>
2024-09-07 14:00:32 +0300

    2

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,10 @@
 fun main() {
-    println("Hello World!")
+
+    var personOne = Person<String, Long>("Виктор", 79287665524)
+    var personTwo = Person<String, String>("Дмитрий","79829838454")
+
+    println("Имя: ${personTwo.name}  Номер телефона: ${personTwo.phone}")
+    println("Имя: ${personOne.name}  Номер телефона: ${personOne.phone}")
 }
+
+class Person<T,V>(val name: T, var phone: V)
